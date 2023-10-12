72d84f2c530d96009fab3a2a96a156ce7a9a1a7a
Nicolás <nick.2004@outlook.com>
2023-10-12 16:58:42 -0500

    Implementación del css y javascript

---
+++ Cronómetro/js/index.js
@@ -0,0 +1,74 @@
+
+
+// Declaración de variables para el cronometro
+let timer;
+let running = false;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// Selección de elementos HTML
+const display = document.getElementById('display');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+// Función para iniciar el cronometro
+function startTimer() {
+    if (!running) {
+        running = true;
+        startButton.innerText = 'Iniciado'; //Al iniciar el cronometro con el boton iniciar, este se desabilita y cambia el texto a iniciado
+        startButton.disabled = true;
+        stopButton.disabled = false;
+        resetButton.disabled = false;
+        // Inicia un intervalo que llama a la función updateTimer cada segundo (1000 ms) que es equivalente a un segundo
+        timer = setInterval(updateTimer, 1000);
+    }
+}
+
+// Función para detener el cronometro
+function stopTimer() {
+    if (running) {
+        running = false;
+        startButton.innerText = 'Iniciar';
+        startButton.disabled = false;
+        stopButton.disabled = true;
+        // Detiene el intervalo de actualización del cronometro
+        clearInterval(timer);
+    }
+}
+
+// Función para reiniciar el cronometro
+function resetTimer() {
+    // Llama a la función stopTimer para asegurarse de que el cronometro esté detenido
+    stopTimer();
+    // Reinicia las variables de tiempo (segundos, minutos y horas) a cero
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    // Actualiza el elemento HTML 'display' para mostrar '00:00:00'
+    display.innerText = '00:00:00';
+    // Desactiva el botón de reinicio
+    resetButton.disabled = true;
+}
+
+// Función para actualizar el cronometro cada segundo
+function updateTimer() {
+    seconds++;
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    // Formatea el tiempo en formato HH:MM:SS y actualiza el elemento HTML 'display' y este se asegura para que el cronometro contenga almenos dos numeros mientras se va actualizando.
+    const displayText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    display.innerText = displayText;
+}
+
+// Agrega event listeners a los botones para que ejecuten las funciones correspondientes cuando se hacen clic en ellos
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
---
+++ Formulario/js/index.js
@@ -0,0 +1,41 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén referencias a elementos del DOM
+    const inputTexto = document.getElementById("inputTexto"); // Campo de entrada de texto
+    const btnAgregar = document.getElementById("btnAgregar"); // Botón de agregar
+    const btnBorrar = document.getElementById("btnBorrar");   // Botón de borrar
+    const lista = document.getElementById("lista");           // Lista no ordenada
+
+    // Agrega un evento de escucha al botón "Agregar"
+    btnAgregar.addEventListener("click", function () {
+        // Obtiene el texto ingresado en el campo de entrada, eliminando espacios en blanco al principio y al final
+        const textoIngresado = inputTexto.value.trim();
+
+        // Verifica si el texto ingresado no está vacío
+        if (textoIngresado !== "") {
+            // Crea un nuevo elemento de lista
+            const li = document.createElement("li");
+
+            // Establece el texto del elemento de lista en minúsculas y lo asigna al elemento de lista
+            li.textContent = textoIngresado.toLowerCase();
+
+            // Agrega el elemento de lista a la lista no ordenada
+            lista.appendChild(li);
+
+            // Limpia el campo de entrada después de agregar el elemento de lista
+            inputTexto.value = "";
+        }
+    });
+
+    // Agrega un evento de escucha al botón "Borrar"
+    btnBorrar.addEventListener("click", function () {
+        // Obtiene una referencia al último elemento de lista
+        const ultimaEntrada = lista.lastChild;
+
+        // Verifica si hay un último elemento en la lista
+        if (ultimaEntrada) {
+            // Elimina el último elemento de la lista
+            lista.removeChild(ultimaEntrada);
+        }
+    });
+});
